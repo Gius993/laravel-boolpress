ccbaa30af526bcc3bfb4b1cb8d10e9170727204e
Gius993 <giuseppevidili9@gmail.com>
2022-09-02 15:10:47 +0200

    home pubblica

--- laravel-project/routes/web.php
+++ laravel-project/routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
@@ -26,4 +23,9 @@ Route::middleware('auth')
 ->group(function(){
     Route::get('/', 'HomeController@index')
     ->name('home');
+});
+
+
+Route::get('/', function () {
+    return view('guest.home');
 });

4896d818420b0a2da16665bc27f9c06b23327723
Gius993 <giuseppevidili9@gmail.com>
2022-09-05 15:12:11 +0200

    popolo la tabella

--- laravel-project/database/seeds/PostsTableSeeder.php
+++ laravel-project/database/seeds/PostsTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use App\Post;
+use Illuminate\Support\Str;
 class PostsTableSeeder extends Seeder
 {
     /**
@@ -13,7 +14,14 @@ class PostsTableSeeder extends Seeder
     public function run(Faker $faker)
     {
         for($i = 0; $i < 10; $i++){
+            $new_post = new Post();
 
+            $new_post->title = ucfirst($faker->words(rand(4, 8), true));
+            $new_post->content = $faker->paragraph(rand(8, 20), true);
+            $new_post->slug = Str::slug($new_post->title, '-');
+
+            $new_post->save();
+            
         }
     }
 }

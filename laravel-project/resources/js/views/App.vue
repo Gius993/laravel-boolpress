a086b6d5a71bef55e72c3b07096daf5340d7fcdb
Gius993 <giuseppevidili9@gmail.com>
2022-09-12 15:09:42 +0200

    creo controller e lo collego con la rota in api

--- laravel-project/resources/js/views/App.vue
+++ laravel-project/resources/js/views/App.vue
@@ -3,24 +3,24 @@
 	<div>
 		<div class="container">
 			<a href="/admin"> va ad admin</a>
-  <div class="row">
-    <div class="col">
-      <div class="card">
-			<h1>CIAO</h1>
-	  </div>
-    </div>
-    <div class="col">
-		<div class="card">
-			<h1>CIAO</h1>
-	  </div>
-    </div>
-    <div class="col">
-		<div class="card">
-			<h1>CIAO</h1>
-	  </div>
-    </div>
-  </div>
-</div>
+ 			<div class="row">
+   				 <div class="col">
+    			  	<div class="card">
+						<h1>CIAO</h1>
+					  </div>
+  				 </div>
+ 			   <div class="col">
+					<div class="card">
+						<h1>CIAO</h1>
+					</div>
+  			  </div>
+  				  <div class="col">
+					<div class="card">
+						<h1>CIAO</h1>
+					 </div>
+   				 </div>
+ 			 </div>
+		</div>
 	</div>
 
 </template>

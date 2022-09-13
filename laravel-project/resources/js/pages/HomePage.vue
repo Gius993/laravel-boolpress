9aae90e47907d085123c27abceac034505357718
Gius993 <giuseppevidili9@gmail.com>
2022-09-13 15:08:53 +0200

    router pronte con blog che mostra posts

---
+++ laravel-project/resources/js/pages/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+ <div>
+	<h3>ciaopooo</h3>
+ </div>
+
+</template>
+
+<script>
+	export default{
+		name: 'HomePage'
+	}
+
+</script>
--- laravel-project/resources/js/views/App.vue
+++ laravel-project/resources/js/views/App.vue
@@ -2,19 +2,19 @@
 
 	<div>
 
-		<Posts />
+		<router-view></router-view>
 
 	</div>
 
 </template>
 
 <script>
-	import Posts from '../components/Posts.vue';
+	
 
 	export default {
 		name: 'App',
 		components: {
-			Posts
+			
 		},
  
 	}
